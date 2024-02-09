2752ad1141bc44f4108d06af7f8a7039eb1322dd
z3lx <57017122+z3lx@users.noreply.github.com>
2024-02-09 09:39:42 -0500

    Add importer config documentation

--- Editor/ImporterConfig.cs
+++ Editor/ImporterConfig.cs
@@ -3,13 +3,43 @@ using UnityEngine;
 
 namespace z3lx.ACGImporter.Editor
 {
+    /// <summary>
+    /// Represents the configuration for the ACGImporter.
+    /// </summary>
     public class ImporterConfig
     {
+        /// <summary>
+        /// Gets or sets the input path for the importer.
+        /// </summary>
         public string InputPath;
+
+        /// <summary>
+        /// Gets or sets the output path for the importer.
+        /// </summary>
         public string OutputPath;
+
+        /// <summary>
+        /// Gets or sets a flag indicating whether to
+        /// create a directory for each material category.
+        /// </summary>
         public bool CreateCategoryDirectory;
+
+        /// <summary>
+        /// Gets or sets a flag indicating whether to
+        /// create a directory for each material.
+        /// </summary>
         public bool CreateMaterialDirectory;
+
+        /// <summary>
+        /// Gets or sets the shader to be used to create
+        /// the material.
+        /// </summary>
         public Shader Shader;
+
+        /// <summary>
+        /// Gets or sets the list of shader properties to
+        /// be set the material.
+        /// </summary>
         public List<ShaderProperty> ShaderProperties;
     }
 }
