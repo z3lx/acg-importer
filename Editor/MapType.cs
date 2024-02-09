e283889eb010632e11f149163c540fce042b7cee
z3lx <57017122+z3lx@users.noreply.github.com>
2024-02-09 09:30:35 -0500

    Add map type documentation

--- Editor/MapType.cs
+++ Editor/MapType.cs
@@ -1,15 +1,55 @@
 ﻿namespace z3lx.ACGImporter.Editor
 {
+    /// <summary>
+    /// Represents a type of map.
+    /// </summary>
     public enum MapType
     {
+        /// <summary>
+        /// Represents a color map.
+        /// </summary>
         Color,
+
+        /// <summary>
+        /// Represents a normal map.
+        /// </summary>
         Normal,
+
+        /// <summary>
+        /// Represents a metallic map.
+        /// </summary>
         Metallic,
+
+        /// <summary>
+        /// Represents a roughness map.
+        /// </summary>
         Roughness,
+
+        /// <summary>
+        /// Represents an occlusion map.
+        /// </summary>
         Occlusion,
+
+        /// <summary>
+        /// Represents a height map.
+        /// </summary>
         Height,
+
+        /// <summary>
+        /// Represents a smoothness map.
+        /// </summary>
         Smoothness,
+
+        /// <summary>
+        /// Represents a metallic gloss map, with
+        /// Metallic (RGB) and Smoothness (A) packed into a single texture.
+        /// </summary>
         MetallicGloss,
+
+        /// <summary>
+        /// Represents a mask map, with
+        /// Metallic (R), Occlusion (G), Detail Mask (B), and Smoothness (A) packed into a single texture.
+        /// </summary>
         Mask
     }
 }
